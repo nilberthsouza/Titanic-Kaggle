5cf97fd21ec2fa37ccf5f252bd091132c7e596b5
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2020-01-04 16:36:31 -0300

    Update and rename analiseExploratoria.md to analiseExploratoria.r

--- analiseExploratoria.r
+++ analiseExploratoria.r
@@ -1,19 +1,11 @@
-```{r}
+
 train <- read.csv(file="Documents/pythonprogram/Codigos-de-Data-Science/train.csv",header =TRUE)
 head(train)
-```
 
-```{r}
 freq <-table(train[,2])
 freq
-```
 
-```{r}
 #grafico de pizza 
 pie(freq,main="sobreviventes",labels=c("61.6%","38.4%"),col=c(4,2))
 legend("bottomleft",fill=c(4,2),legend=c("mortos","sobreviventes"))
-```
-```{r}
 
-
-```
