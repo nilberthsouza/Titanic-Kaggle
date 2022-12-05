c851cd78987d710ebd7326cd8a798ae6ae6a27dd
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2022-12-05 00:21:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,11 @@ plt.subplot2grid((2,3),(0,2))
 df.Pclass.value_counts(normalize=True).plot(kind="bar")
 plt.title("Class")
 
-
 plt.subplot2grid((2,3),(1,0),colspan=2)
 for x in [1,2,3]:
     df.Age[df.Pclass == x].plot(kind="kde")
 plt.title("Class wrt Age")
 plt.legend(("1st","2nd","3rd"))
-
 
 plt.subplot2grid((2,3),(1,2))
 df.Embarked.value_counts(normalize=True).plot(kind="bar")
